a417e520bbcba9341bc4e7d754098455659ef9fd
Ryananta Danendra <ryanantadanendra887@gmail.com>
2026-09-24 14:17:29 +0800

    adding login page user logic

--- frontend/app/(login-admin)/login/page.tsx
+++ frontend/app/(login-admin)/login/page.tsx
@@ -2,8 +2,22 @@
 
 import { Suspense } from "react";
 import LoginForm from "@/components/LoginForm";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/hooks/auth";
+import { useEffect } from "react";
 
 const Login = () => {
+  const router = useRouter();
+  const { user } = useAuth({ middleware: "auth" });
+
+  useEffect(() => {
+    if (user === null) {
+      router.push("/login");
+    } else if (user) {
+      router.push("/dashboard");
+    }
+  }, [user, router]);
+
   return (
     <Suspense fallback={<div>Memuat...</div>}>
       <LoginForm />
